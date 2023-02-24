6c0f4f3f55de6224f5824d127ae3eda30d154420
KANABO\Huindai <liam.v.swain@hotmail.com>
2023-02-24 16:18:46 -0600

    Client Registration Page

--- src/app/client-registration/client-registration.component.ts
+++ src/app/client-registration/client-registration.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-client-registration',
   templateUrl: './client-registration.component.html',
   styleUrls: ['./client-registration.component.css']
 })
-export class ClientRegistrationComponent {
+export class ClientRegistrationComponent implements OnInit{
+  public notFilled: boolean = false;
+  public passwordMismatch: boolean = false;
+  onSubmit(formData: any)
+  {
+    this.notFilled = false
+    this.passwordMismatch = false;
+    console.log(formData);
+    if(formData['username'] == '' || formData['password'] == '')
+    {
+      this.notFilled = true;
+    }
+
+    else if(formData['password'] != formData['confirmedPassword']){
+      this.passwordMismatch = true;
+    }
+
+
+  }
+
+
+  ngOnInit(): void {
+  }
 
 }
