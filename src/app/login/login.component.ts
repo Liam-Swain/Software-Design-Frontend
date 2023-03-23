416164423acfe92c6bed3536ba3676ff32965011
PaulEdson <pauledson98@yahoo.com>
2023-03-23 15:01:59 -0500

    Update login.component.ts

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -10,25 +11,34 @@ export class LoginComponent implements OnInit{
   private username: string = '';
   private password: string = '';
   public errorString: string = '';
+  private userObject: any;
   
-  constructor() {}
+  constructor(private http: HttpClient) {};
 
   onSubmit(formData: any)
   {
     this.username = formData['username'];
     this.password = formData['password'];
 
-    this.errorString = this.checkLogin();
+    this.errorString = this.checkLogin(formData);
+
   }
 
   //returns error string for when user enters wrong login
-  checkLogin(){
+  checkLogin(formData: any){
+    this.http.post("http://localhost:9196/softwaredesign/verifyLogin", formData).subscribe(res => {
+        this.userObject = res;
+        console.log(this.userObject);
+      })
     if(this.username == '' || this.password == ''){
       return 'You have not entered a username or password'
     }
+    else if(this.userObject){
+      return 'incorrect user or password'
+    }
     // elif statement to check database for matching login info would go here
     else{
-      return  ''
+      return  'logged in'
     }
   }
   
