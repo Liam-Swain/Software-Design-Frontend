43a22ade37a09442d9f704d1a430e55d119b5a28
KANABO\Huindai <liam.v.swain@hotmail.com>
2023-04-14 15:43:27 -0500

    Updated pricing module

--- src/app/fuel-quote/fuel-quote.component.ts
+++ src/app/fuel-quote/fuel-quote.component.ts
@@ -6,26 +6,55 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './fuel-quote.component.html',
   styleUrls: ['./fuel-quote.component.css']
 })
-export class FuelQuoteComponent implements OnInit{
+export class FuelQuoteComponent implements OnInit {
 
   public notFilled: boolean = false;
+  public gotPrice: boolean = false;
+  public currentUser: any;
+  public displayError: boolean = false;
+  response: any;
+  constructor(private http: HttpClient) { }
 
-  constructor(private http: HttpClient) {}
 
-  onSubmit(formData: any)
-  {
+  getPrice() {
+    this.displayError = false;
+    var gallon: any = document.getElementById("gallons");
+    var date: any = document.getElementById("date");
+    gallon = gallon.value;
+    date = date.value;
+    if (gallon == "" || gallon == "" || date == "" || date == "") {
+      this.displayError = true;
+    }
+    else {
+      var request = {
+        'gallonsRequested': gallon,
+        'date': date,
+        'username': this.currentUser['user'],
+        'state': this.currentUser['state']
+      }
+      this.http.post("http://localhost:9196/softwaredesign/createQuote", request).subscribe(res => {
+        this.response = res;
+        console.log(this.response);
+        this.gotPrice = true;
+      });
+    }
+
+
+
+  }
+
+  onSubmit(formData: any) {
     this.notFilled = false;
     console.log(formData);
 
-    if(formData['gallons'] == '' || formData['date'] == '')
-    {
+    if (formData['gallons'] == '' || formData['date'] == '') {
       this.notFilled = true;
     }
 
     this.http.post("http://localhost:9196/softwaredesign/quoteHistory/user", formData).subscribe(res => {
-    console.log(res);
+      console.log(res);
 
-  });
+    });
 
   }
 
@@ -37,7 +66,9 @@ export class FuelQuoteComponent implements OnInit{
 
 
   ngOnInit(): void {
-    
+    this.currentUser = sessionStorage.getItem("userInfo");
+    this.currentUser = JSON.parse(this.currentUser);
+    console.log(this.currentUser);
   }
 
 
